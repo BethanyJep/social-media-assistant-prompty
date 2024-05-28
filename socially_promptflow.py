0d6de1410e6ca5eb992b4a2245af626bde68e962
Pikachú <engbethany4@gmail.com>
2024-05-28 14:20:08 +0300

    first commit

---
+++ socially_promptflow.py
@@ -0,0 +1,41 @@
+import json
+from promptflow.core import tool
+from promptflow.core import Prompty
+
+from pathlib import Path
+folder = Path(__file__).parent.absolute().as_posix()
+
+@tool
+def flow_entry(    
+      blog_title: any,
+      blog_link: any,
+      call_to_action: any,
+      post_type: any
+) -> str:
+  # path to prompty (requires absolute path for deployment)
+  path_to_prompty = folder + "/socially.prompty"
+
+  # load prompty as a flow
+  flow = Prompty.load(path_to_prompty)
+ 
+  # execute the flow as function
+  result = flow(
+    blog_title = blog_title,
+    blog_link = blog_link,
+    call_to_action = call_to_action,
+    post_type = post_type
+  )
+
+  return result
+
+if __name__ == "__main__":
+   json_input = '''{
+  "blog_title": "LLM based development tools - PromptFlow vs LangChain vs Semantic Kernel",
+  "blog_link": "https://techcommunity.microsoft.com/t5/educator-developer-blog/llm-based-development-tools-promptflow-vs-langchain-vs-semantic/ba-p/4149252",
+  "call_to_action": "GitHub Sample Code at https://github.com/BethanyJep/Swahili-Tutor",
+  "post_type": "Twitter"
+}'''
+   args = json.loads(json_input)
+
+   result = flow_entry(**args)
+   print(result)
